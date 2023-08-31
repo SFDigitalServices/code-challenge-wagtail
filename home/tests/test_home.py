b0a0af10cdcb62ef2cbd3093c801b195967170b4
Derek Chinn <derek.chinn@sfgov.org>
2023-08-31 13:11:50 -0700

    Adding pytest

--- home/tests/test_home.py
+++ home/tests/test_home.py
@@ -1,6 +1,9 @@
 from wagtail.test.utils import WagtailPageTestCase
-from home.models import HomePage
+# from home.models import HomePage
 
 class TestHomePage(WagtailPageTestCase):
-    def test_can_create_page(self):
+    def test_success(self):
         assert True == True
+
+    def test_fail(self):
+        assert True == False
